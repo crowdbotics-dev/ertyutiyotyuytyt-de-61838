e8c00c1bd0c1be7b93f453c1cf283c294aa03f18
autofirst autosecond <cbautomation@crowdbotics.com>
2023-02-07 18:29:02 +0530

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "ertyutiyotyuytyt-de-61838",
   url: "https://ertyutiyotyuytyt-de-61838.botics.co",
   api: "https://ertyutiyotyuytyt-de-61838.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
